edc5e3886d5a35b1626963f613843aa3e8524bcd
Li.Meng <1784238945@qq.com>
2021-10-28 18:27:34 +0800

    add http api metrics code

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+//多个命令行参数使用 接口定义
 type Value interface {
 	String() string
 	Set(string) error
@@ -28,24 +29,30 @@ func (i *arrayFlags) Set(value string) error {
 }
 
 func main() {
+	// 定义命令行参数信息
 	var modules arrayFlags
 	flag.Var(&modules, "module", "Some config metrics module name")
 	flag.Parse()
+
+	// 判断传入的是否为空，否则抛出异常
 	if modules == nil {
 		log.Fatal("Please enter the parameters [module] !")
 	}
 
+	// 定时任务执行
 	t := time.Duration(config.ReadConfig("timed_task.second").(int))
-	ticker := time.NewTicker(t * time.Second)
-	defer ticker.Stop() // 定时任务执行
+	ticker := time.NewTicker(t * time.Second) // 定时任务执行间隔，config.yaml里配置
+	defer ticker.Stop()                       // 执行完启动阻塞
 	for range ticker.C {
 		for _, m := range modules {
 			urls := config.ReadConfig("modules." + m + ".url").([]interface{})
 			mothod := config.ReadConfig("modules." + m + ".mothod").(string)
 
+			// 连接数据库
 			conn := module.Conninflux()
 			defer conn.Close()
 
+			//判断mothod方法
 			for _, url := range urls {
 				if mothod == "GET" {
 					trace := module.Get_Trace(url.(string))
